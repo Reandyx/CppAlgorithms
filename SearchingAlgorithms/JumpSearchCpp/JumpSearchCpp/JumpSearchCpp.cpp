b27fec8b9b716bdd4dd5e65808a00e948d4a9e35
Reandyx <reandyxx@gmail.com>
2022-01-19 09:04:15 +0100

    Add files via upload

---
+++ SearchingAlgorithms/JumpSearchCpp/JumpSearchCpp/JumpSearchCpp.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<math.h> //cmath
+
+using namespace std;
+
+int jumpSearch(int arr[], int n, int value)
+{
+    int i, right, left, jump = (int)sqrt(n);
+
+    for (i = 0; i + jump < n && arr[i] < value; i += jump) {
+        // Keep jumping until value is between any two
+        // elements of the array or when i goes out of bound
+    }
+
+    if (value <= arr[i]) {
+        // right is i if search value is less than
+        // any jumped element in the array.
+        right = i;
+    }
+    else {
+        // If not, right is last element of the array 
+        right = n;
+    }
+
+    left = max(right - jump, 0);    // ensuring left doesn't go negative
+
+    // Linear search backwards
+    while (right >= left) {
+        if (arr[right--] == value) {        //after jump linear search
+            return right + 1;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int arr[] = { 1, 3, 5, 7, 9, 11, 13, 15 };      //in the future to add custom sizeable 
+    int n = sizeof(arr) / sizeof(int);
+    //output and input
+    cout << jumpSearch(arr, n, 0) << endl; // -1
+    cout << jumpSearch(arr, n, 3) << endl; // 1
+    cout << jumpSearch(arr, n, 11) << endl; // 5
+    cout << jumpSearch(arr, n, 16) << endl; // -1
+
+    return 0;
+}
