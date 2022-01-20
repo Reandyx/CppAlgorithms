d8e0c08d178f1ffb3247bac4cd4b5dabf307ae3b
Reandyx <reandyxx@gmail.com>
2022-01-20 22:10:34 +0100

    Add files via upload

---
+++ Sorting Algorithms/MergeSortCpp/MergeSortCppV2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+ 
+template<class T>
+void merge(std::vector<T> &vec, int low, int mid, int high)
+{
+    int index = low;
+    std::queue<T> left;
+    std::queue<T> right;
+ 
+    for (int i = low; i <= mid; i++) {
+        left.push(vec[i]);
+    }
+    for (int i = mid + 1; i <= high; i++) {
+        right.push(vec[i]);
+    }
+ 
+    while (!left.emtpy() && !right.empty()) {
+        if (left.front() < right.front()) {
+            vec[index] = left.front();
+            left.pop();
+        }
+        else {
+            vec[index] = right.front();
+            right.pop();
+        }
+        index++;
+    }
+    while (!left.empty()) {
+        vec[index] = left.front();
+        left.pop();
+        index++;
+    }
+ 
+    while (!right.empty()) {
+        vec[index] = right.front();
+        right.pop();
+        index++;
+    }
+     
+}
+ 
+template<class T>
+void merge_sort(std::vector<T> &vec, int low, int high)
+{
+    if (low < high) {
+        int middle = (low + high) / 2;
+        merge_sort(vec, low, middle);
+        merge_sort(vec, middle + 1, high);
+        merge(vec, low, middle, high);
+    }
+}
+
