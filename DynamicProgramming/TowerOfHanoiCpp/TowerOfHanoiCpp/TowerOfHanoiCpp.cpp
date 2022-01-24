93523223f2645b1df2831f9a5bd539cef8e79eef
Reandyx <reandyxx@gmail.com>
2022-01-24 22:06:29 +0100

    Add files via upload

---
+++ DynamicProgramming/TowerOfHanoiCpp/TowerOfHanoiCpp/TowerOfHanoiCpp.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using std::cout;
+using std::endl;
+
+void TowerOfHanoi(int N)
+{
+	// Iterate over the range <0, 2^N - 1>
+	for (int x = 1;
+		x <= pow(2, N) - 1; x++) {
+		cout << "Move from Rod " << ((x & x - 1) % 3 + 1) << " to Rod " << (((x | x - 1) + 1) % 3 + 1) << endl;
+	}
+}
+
+int main()
+{
+	int N = 3;
+	TowerOfHanoi(N);
+	return 0;
+}
