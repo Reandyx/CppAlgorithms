412621d4a0a1b52402c2deba87ee511660c2be99
Reandyx <reandyxx@gmail.com>
2022-01-24 23:22:51 +0100

    Add files via upload

---
+++ RecursiveProgramming/ExpotentialRecursionCpp/ExpotentialRecCpp/ExpotentialRecCpp/ExpotentialRecCpp.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using std::cout;
+
+int exponentMod(int A, int B, int C)
+{
+	if (A == 0)
+		return 0;
+	if (B == 0)
+		return 1;
+
+	long y;
+	if (B % 2 == 0) {
+		y = exponentMod(A, B / 2, C);
+		y = (y * y) % C;
+	}
+
+	else {
+		y = A % C;
+		y = (y * exponentMod(A, B - 1, C) % C) % C;
+	}
+
+	return (int)((y + C) % C);
+}
+
+int main()
+{
+	int A = 2, B = 5, C = 13;
+	cout << "Power is " << exponentMod(A, B, C);
+	return 0;
+}
+
---
+++ RecursiveProgramming/ExpotentialRecursionCpp/ExpotentialTaylorCPp/ExpotentialTaylorCPp/ExpotentialTaylorCPp.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using std::cout;
+
+double e(int x, int n)
+{
+	static double p = 1, f = 1;
+	double r;
+
+	if (n == 0)
+		return 1;
+
+	r = e(x, n - 1);
+	p = p * x;
+	f = f * n;
+	return (r + p / f);
+}
+
+int main()
+{
+	int x = 4, n = 15;	//main parameters
+	cout << "\n" << e(x, n);
+	return 0;
+}
+
